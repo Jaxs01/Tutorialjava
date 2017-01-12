f8abbc396da462f78bbef98446986045f4961c18
Jacqueline <Jacqueline.prange@assurity.co.nz>
2017-01-12 13:06:08 +1300

    Step 5

---
+++ src/com/tamingjava/Main.java
@@ -0,0 +1,14 @@
+//1. Hello world ...compile with javac
+//2. create a packagetamingjava
+//3.
+package com.tamingjava;
+
+/**
+ * Created by Jacqueline on 16/12/2016....
+ */
+ 
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello, Cillin");
+    }
+}
---
+++ src/com/tamingjava/Print.java
@@ -0,0 +1,13 @@
+//class Print
+//
+package tamingjava;
+
+/**
+ * Created by Jaxs on 12/01/2017....
+ */
+ 
+public class Print {
+    public static void main (String[] args) {
+        System.out.println("Have a fabulous fantastic Day");
+    }
+}
