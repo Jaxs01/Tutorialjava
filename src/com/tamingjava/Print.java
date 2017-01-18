0e6b26138c3d91acab529ac26f80eb971363451c
Jacqueline <Jacqueline.prange@assurity.co.nz>
2017-01-18 17:13:12 +1300

    requested changes included

--- src/com/tamingjava/Print.java
+++ src/com/tamingjava/Print.java
@@ -1,7 +1,8 @@
-//Step6 Add a new Print.java class in com.tamingjava.print package
-//Add a static method in this class that accepts a String param and prints it
-//Step7 Call Print.java class from Main.java to print hello Jaxs, how are you today
-
+/*
+* Step6 Add a new Print.java class in com.tamingjava.print package
+* Add a static method in this class that accepts a String param and prints it
+* Step7 Call Print.java class from Main.java to print hello Jaxs, how are you today
+*/
 package com.tamingjava;
 
 /**
@@ -12,6 +13,11 @@ public class Print
 {
     public static void print (String printable) 
 	{
+/* 
+* add \n for new line 
+*     \t for tabulator
+* and another line printed
+*/
        		System.out.println("\" Hello " + printable + ",\n \t how are you today?\"");
 			System.out.println("*              Have a wonderful day           *");
     }
