d41bc69d43747e344e7328148f86f52f8de8c1da
Jacqueline <Jacqueline.prange@assurity.co.nz>
2017-01-18 16:18:29 +1300

    Step09

--- src/com/tamingjava/Print.java
+++ src/com/tamingjava/Print.java
@@ -1,6 +1,6 @@
-//Step6: Add a new Print.java class in com.tamingjava.print package. 
-//Add a static method in this class that accepts a String param and prints it.
-//Step7:Step 7: Call Print.java class from Main.java to print “hello Jaxs have a fabulous fantastic day”.
+//Step6 Add a new Print.java class in com.tamingjava.print package
+//Add a static method in this class that accepts a String param and prints it
+//Step7 Call Print.java class from Main.java to print hello Jaxs, how are you today
 
 package com.tamingjava;
 
@@ -12,6 +12,7 @@ public class Print
 {
     public static void print (String printable) 
 	{
-       		System.out.println("\"Hello " + printable + ",have a fabulous fantastic day\"");
+       		System.out.println("\" Hello " + printable + ",\n \t how are you today?\"");
+			System.out.println("*              Have a wonderful day           *");
     }
 }	
